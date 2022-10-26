b929ee02517c64de4dafad69060fb824e28f1ab7
Niclas Forsman <niclas.forsman@futurice.com>
2022-10-26 16:28:30 +0300

    add initial state for game room

--- server/src/game/room.py
+++ server/src/game/room.py
@@ -1,10 +1,30 @@
 import time
+import random
+
+room_state = {
+    "player_1_id": random.getrandbits(32),
+    "player_1_pos": 0.5,
+    "ball_pos": (0.5, 0.5),
+    "state": 'waiting',
+    "score": (0, 0)
+}
 
 
 def start(id, state):
     print(f'starting game room %s' % id)
-    print('')
-    # poll queue in shared memory
+    room_state["room_id"] = id
+    run(state)
+
+
+def connect():
+    if room_state["state"] != 'playing':
+        room_state["player_2_pos"]: 0.5
+        room_state["player_2_id"]: random.getrandbits(32)
+        room_state['playing']
+
+
+def run(state):
+    print('asd')
     while True:
         time.sleep(5)
         print(state)
@@ -14,5 +34,3 @@ def start(id, state):
                 print(f'state is %s' % n)
                 print(state)
                 # break
-    # do logic
-    return None
